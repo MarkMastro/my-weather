09f51cd1a05f2e99b6aeb729c7184583ad79d505
MarkMastro <mastropaolo.mark@gmail.com>
2022-04-07 22:53:56 -0400

    display city

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,15 +1,20 @@
 from flask import Flask
 import requests
-
+import json 
 app = Flask(__name__)
 
 #API Route
 
 @app.route("/weather", methods={'GET'})
 def index():
-    req=requests.get('https://wttr.in/')
-    print(req.content)
-    return req.content
+    req=requests.get('https://wttr.in/?format=j1')
+
+    weather_dict = json.loads(req.content.decode("utf-8"))
+
+
+
+    
+    return weather_dict["nearest_area"][0]["areaName"][0]["value"]
 
 if __name__ == "__main__":
     app.run(debug=True)
