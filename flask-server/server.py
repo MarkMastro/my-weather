5add7de36a8cfe2ab8aa9151e10821ec297a527d
MarkMastro <mastropaolo.mark@gmail.com>
2022-04-08 20:10:42 -0400

    get weather information and store in dictionary of values

--- flask-server/server.py
+++ flask-server/server.py
@@ -9,12 +9,30 @@ app = Flask(__name__)
 def index():
     req=requests.get('https://wttr.in/?format=j1')
 
-    weather_dict = json.loads(req.content.decode("utf-8"))
+    req_dict = json.loads(req.content.decode("utf-8"))
 
+    temp_C = float(req_dict["current_condition"][0]["temp_C"])
+    wind_speed =float(req_dict["current_condition"][0]["windspeedKmph"]) 
+    print(type (temp_C))
+    print(type (wind_speed))
+
+    wind_chill = round(13.12 + 0.6215*temp_C - 11.37 *(wind_speed**0.16) + 0.3965*temp_C* (wind_speed**0.16))
+
+
+    weather_info = {
+        "City" : req_dict["nearest_area"][0]["areaName"][0]["value"],
+        "Country" : req_dict["nearest_area"][0]["country"][0]["value"],
+        "temp_c" : temp_C,
+        "wind_speed" : wind_speed,
+        "wind_chill" : wind_chill
+    }
+
+    print(weather_info)
+
+    
 
 
     
-    return weather_dict["nearest_area"][0]["areaName"][0]["value"]
-
+    return req_dict
 if __name__ == "__main__":
     app.run(debug=True)
