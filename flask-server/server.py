f249c6bf5f70834704108c50c1a719dbf4cccf42
MarkMastro <mastropaolo.mark@gmail.com>
2022-04-08 20:26:53 -0400

    created switch object to return clothing suggestion depending on wind_chill

--- flask-server/server.py
+++ flask-server/server.py
@@ -27,12 +27,36 @@ def index():
         "wind_chill" : wind_chill
     }
 
+
+    class Switch(dict):
+        def __getitem__(self, item):
+            for key in self.keys():                 # iterate over the intervals
+                if item in key:                     # if the argument is in that interval
+                    return super().__getitem__(key) # return its associated value
+            raise KeyError(item) 
+
+
+    switch = Switch({
+        range(-100,-20): "It's extremely cold out, consider staying inside.",
+        range(0,10): "It's cold out, wear a winter jacket, light hat, and gloves.",
+        range(10,17): "It's cool out, wear a light jacket.",
+        range(17,22): "It's comfortable out, wear whatever you want.",
+        range(22,27): "It's warm out, consider a t-shirt and pants."
+    })
+
+    clothing = switch[wind_chill]
+    
     print(weather_info)
 
-    
+    weather_info = {
+        "City" : req_dict["nearest_area"][0]["areaName"][0]["value"],
+        "Country" : req_dict["nearest_area"][0]["country"][0]["value"],
+        "temp_c" : temp_C,
+        "wind_speed" : wind_speed,
+        "wind_chill" : wind_chill,
+        "clothing" : clothing
+    }
 
-
-    
-    return req_dict
+    return weather_info
 if __name__ == "__main__":
     app.run(debug=True)
